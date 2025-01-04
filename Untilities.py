b2abe5db1b9f5aebd7c71c8486e8aecdecdeb6f2
MurMikeS <mikethsong@gmail.com>
2025-01-04 00:46:41 -0500

    Update the watershad method by adding nuclei mask

--- Untilities.py
+++ Untilities.py
@@ -1,7 +1,7 @@
 ## Image segmentation and analysis from breast cancer cells for Mitochondria
 #
 #
-# Tzu-Hsi October 2024
+# Tzu-Hsi Dec 2024
 import os
 import cv2
 import sys
@@ -12,11 +12,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from skimage.feature import peak_local_max
 from skimage.measure import label, regionprops
+from scipy.spatial.distance import cdist
 from scipy import ndimage
 from skimage.segmentation import watershed
 
-
-# Check the input file format
 def check_file_format(data_path):
     img_format = os.listdir(data_path)
     check_img = img_format[0]
@@ -35,7 +34,6 @@ def check_file_format(data_path):
 
     return imgformat
 
-# Read the input image data
 def Get_Image_Data(data_path, Sample_index, type):
     imgformat = check_file_format(data_path)
     if imgformat:
@@ -49,7 +47,6 @@ def Get_Image_Data(data_path, Sample_index, type):
         print('Please ensure the file is in one of the following formats: .png, .jpg, .jpeg, .tif, or .tiff')
         sys.exit()
 
-# Enhance the image intensity contrast
 def Brightness_Enhance(Img):
     hsv = cv2.cvtColor(Img, cv2.COLOR_BGR2HSV)
     h, s, v = cv2.split(hsv)
@@ -59,7 +56,6 @@ def Brightness_Enhance(Img):
 
     return BE_Img
 
-# Fill the hole
 def imfill_function(mask):
     im_floodfill = mask.copy()
     mask_new = np.zeros((mask.shape[0]+2, mask.shape[1]+2), dtype=np.uint8)
@@ -69,9 +65,13 @@ def imfill_function(mask):
 
     return im_out
 
-# Generate the cell mask
-def Cell_Mask_Generate(cell_mask, cell_distance_set, element_region_size,  cell_intensity_thresh, cell_mask_ORG, cell_mask_contour,advanced_seg, save_sign, save_path, Sample_name):
+def Cell_Mask_Generate(cell_mask, cell_distance_set, nuclei_mask, Suitable_Nuclei_distance_set, element_region_size, cell_intensity_thresh, cell_mask_ORG, cell_mask_contour,advanced_seg, save_sign, save_path, Sample_name):
     # Morphological operations for initial noise remove
+
+    _, th_nuclei_mask = cv2.threshold(nuclei_mask, 0, 255, cv2.THRESH_BINARY)
+    th_nuclei_mask = imfill_function(th_nuclei_mask)
+    th_nuclei_mask = cv2.morphologyEx(th_nuclei_mask, cv2.MORPH_OPEN, kernel=np.ones((5, 5), np.uint8), iterations=5)
+
     cell_mask = 255 - cell_mask
     _, th_cell_mask = cv2.threshold(cell_mask, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)
     th_cell_mask = imfill_function(th_cell_mask)
@@ -79,12 +79,50 @@ def Cell_Mask_Generate(cell_mask, cell_distance_set, element_region_size,  cell_
 
     # Separate connected cells =========================================================================================
     # Use Marker-Controlled Watershed method
-    dist_transform = cv2.distanceTransform(th_cell_mask, cv2.DIST_L2, 5)
-    localMax = peak_local_max(dist_transform, min_distance=cell_distance_set, labels=th_cell_mask)
-    localMax_mask = np.zeros(dist_transform.shape, dtype=bool)
-    localMax_mask[tuple(localMax.T)] = True
-    Cell_markers = ndimage.label(localMax_mask, structure=np.ones((3, 3)))[0]
-    Cell_labels = watershed(-dist_transform, Cell_markers, mask=th_cell_mask)
+    # Use Nuclei centroids as the points
+    # It was used by Local Maximum points being depended on the cell segmentation results
+    Cell_dist_transform = cv2.distanceTransform(th_cell_mask, cv2.DIST_L2, 5)
+    Cell_localMax = peak_local_max(Cell_dist_transform, min_distance=cell_distance_set, labels=th_cell_mask)
+
+    Nuclei_dist_transform = cv2.distanceTransform(th_nuclei_mask, cv2.DIST_L2,5)
+    Nuclei_localMax = peak_local_max(Nuclei_dist_transform, min_distance=Suitable_Nuclei_distance_set, labels=th_nuclei_mask)
+
+    Nuclei_extra_point = []
+    for c_l_ind in Cell_localMax:
+        distances = cdist([c_l_ind], Nuclei_localMax, metric='euclidean')
+        Check_distances = distances < (element_region_size/2)
+        has_false = True in Check_distances
+        if not has_false:
+            if th_nuclei_mask[c_l_ind[1], c_l_ind[0]] > 0:
+                Nuclei_extra_point.append(c_l_ind)
+    Nuclei_extra_point = np.array(Nuclei_extra_point)
+
+    if len(Nuclei_extra_point) != 0:
+        Nuclei_localMax = np.vstack((Nuclei_localMax, Nuclei_extra_point))
+
+    Nuclei_localMax_mask = np.zeros(Nuclei_dist_transform.shape, dtype=bool)
+    Nuclei_localMax_mask[tuple(Nuclei_localMax.T)] = True
+    Nuclei_markers = ndimage.label(Nuclei_localMax_mask, structure=np.ones((3, 3)))[0]
+    Nuclei_labels = watershed(-Nuclei_dist_transform, Nuclei_markers, mask=th_nuclei_mask)
+
+    new_Nuclei_mask = np.zeros(Nuclei_labels.shape, dtype='uint8')
+    nuclei_max_label = np.unique(Nuclei_labels)
+    for n_i in nuclei_max_label:
+        if n_i > 0:
+            tmp_nuclear_mask = np.zeros(Nuclei_labels.shape, dtype='uint8')
+            tmp_nuclear_mask[Nuclei_labels==n_i] = 255
+            tmp_nuclear_mask = cv2.morphologyEx(tmp_nuclear_mask, cv2.MORPH_ERODE, kernel=np.ones((5, 5), np.uint8), iterations=1)
+            tmp_nuclear_mask_label = label(tmp_nuclear_mask)
+            tmp_prop = regionprops(tmp_nuclear_mask_label)
+            if tmp_prop != []:
+                tmp_center = tmp_prop[0].centroid
+                tmp_center = np.round(np.array(tmp_center))
+                tmp_center = tmp_center.astype('int')
+                if th_cell_mask[tmp_center[0], tmp_center[1]] != 0:
+                    new_Nuclei_mask = new_Nuclei_mask+tmp_nuclear_mask
+
+    Cell_markers = ndimage.label(new_Nuclei_mask, structure=np.ones((3, 3)))[0]
+    Cell_labels = watershed(-Cell_dist_transform, Cell_markers, mask=th_cell_mask)
 
     # Extract the separate lines
     new_cell_mask = np.zeros(th_cell_mask.shape, dtype=np.uint8)
@@ -140,7 +178,6 @@ def Cell_Mask_Generate(cell_mask, cell_distance_set, element_region_size,  cell_
 
     return th_cell_mask, separate_line
 
-# Generate the cell mask by using advanced segmentation method (Marker-Control watershed)
 def Advanced_Cell_Mask_Regenerate(new_mask, mos_distance_set, th_cell_mask, cell_mask_ORG, cell_mask_contour, save_sign, save_path, Sample_name):
 
     new_mask_cell = cv2.morphologyEx(new_mask, cv2.MORPH_OPEN, kernel=np.ones((3, 3), np.uint8), iterations=2)
@@ -206,7 +243,7 @@ def Advanced_Cell_Mask_Regenerate(new_mask, mos_distance_set, th_cell_mask, cell
 
     return th_cell_mask, new_mask, advanced_separate_line
 
-# Generate the Mito mask
+
 def Mito_Mask_Generate(mos_mask, th_cell_mask, separate_line, advanced_seg, save_sign, mos_distance_set, cell_mask_ORG, cell_mask_contour, save_path, Sample_name):
     # Generate the initial mos mask and assign the cell labels for removing non-target mos
     binary_threshold, image_result = cv2.threshold(mos_mask, 1, 255, cv2.THRESH_BINARY)
@@ -253,7 +290,6 @@ def Mito_Mask_Generate(mos_mask, th_cell_mask, separate_line, advanced_seg, save
 
     return image_result, new_mask, sep_new_mask, label_num
 
-# Analyze the Mito fragment information
 def Fragment_Analysis(new_mask, Frag_Area_set, sep_new_mask, label_num, mos_mask_contour, save_sign, save_path, Sample_name, image_result):
     frag_label_img = label(new_mask)
     frag_regions = regionprops(frag_label_img)
@@ -285,16 +321,26 @@ def Fragment_Analysis(new_mask, Frag_Area_set, sep_new_mask, label_num, mos_mask
         tmp_min_top = 0
         tmp_min_left = 0
         for sm_i, sm_element in enumerate(tmp_new_single_region):
-            _, _, maxr, maxc = sm_element.bbox
+            minr, minc, maxr, maxc = sm_element.bbox
             if sm_i == 0:
-                tmp_min_top = maxc
-                tmp_min_left = maxr
+                tmp_min_top = minc
+                tmp_min_left = minr
+                tmp_max_top = maxc
+                tmp_max_left = maxr
             else:
-                if tmp_min_top > maxc:
-                    tmp_min_top = maxc
-                if tmp_min_left > maxr:
-                    tmp_min_left = maxr
-        text_location.append([tmp_min_top, tmp_min_left])
+                if tmp_max_top < maxc:
+                    tmp_max_top = maxc
+                if tmp_max_left < maxr:
+                    tmp_max_left = maxr
+                if tmp_min_top > minc:
+                    tmp_min_top = minc
+                if tmp_min_left > minr:
+                    tmp_min_left = minr
+                    
+        if sep_new_mask[tmp_min_top, tmp_min_left] != 0:
+            text_location.append([tmp_max_top, tmp_min_left])
+        else:
+            text_location.append([tmp_min_top, tmp_min_left])
 
     # draw the contour of the mos in each cell and others ==============================================================
 
@@ -367,7 +413,6 @@ def Fragment_Analysis(new_mask, Frag_Area_set, sep_new_mask, label_num, mos_mask
 
     return new_mask_Green, new_mask_Red, color_setting
 
-# Plot the Mito fragment information
 def Frag_Analysis_Plot(mos_mask, sep_new_mask, new_mask_Red, label_num, color_setting, save_sign, save_path, Sample_name):
     frag_int = mos_mask[new_mask_Red == 255]
     sep_cell_only_frag = np.multiply(sep_new_mask, new_mask_Red/255)
@@ -437,7 +482,6 @@ def Frag_Analysis_Plot(mos_mask, sep_new_mask, new_mask_Red, label_num, color_se
 
     # Plot the unfragmentation analysis ================================================================================
 
-# Plot the Mito unfragment information
 def UnFrag_Analysis_Plot(mos_mask, sep_new_mask, new_mask_Green, label_num, frag_number_per_cell, color_setting, save_sign, save_path, Sample_name):
 
     unfrag_int = mos_mask[new_mask_Green == 255]
@@ -500,7 +544,6 @@ def UnFrag_Analysis_Plot(mos_mask, sep_new_mask, new_mask_Green, label_num, frag
 
     return unfrag_int, unfrag_area_per_cell, unfrag_number_per_cell, unfrag_int_per_cell
 
-# Plot the intensity comparison between frag and unfrag
 def Intensity_Comparison(frag_int, unfrag_int, save_sign, save_path, Sample_name):
     all_intensity = [np.mean(frag_int), np.mean(unfrag_int)]
     all_x = np.arange(len(all_intensity))
@@ -517,9 +560,7 @@ def Intensity_Comparison(frag_int, unfrag_int, save_sign, save_path, Sample_name
         plt.close(fig)
 
 
-# Main Work ============================================================================================================
-
-def main(all_data_path, **kwargs):
+def main(all_data_path, update_progress, **kwargs):
     """
 
     Input:
@@ -530,7 +571,8 @@ def main(all_data_path, **kwargs):
                           default=False
             element_region_size = Integer, the treshold value to remove small area, default=2500
             Frag_Area_set: Integer, the treshold value to select which belongs to Fragment, default=1500
-            cell_distance_set: Integer, how far the cell can be considered for segmentation, default=50
+            Suitable_Nuclei_distance_set: Integer, how far the nuclear centroid obtained by distance map can be considered for segmentation, default=80
+            cell_distance_set: Integer, how far the cell obtained by distance map can be considered for segmentation, default=100
             mos_distance_set: Integer, how far the mito can be considered for segmentation, default=100
             cell_intensity_thresh: Integer, the threshold value of intensity for cell region identification, default=8
 
@@ -542,7 +584,8 @@ def main(all_data_path, **kwargs):
     advanced_seg = kwargs.get('advanced_seg', False)
     element_region_size = kwargs.get('element_region_size', 2500)
     Frag_Area_set = kwargs.get('Frag_Area_set', 1500)
-    cell_distance_set = kwargs.get('cell_distance_set', 50)
+    Suitable_Nuclei_distance_set = kwargs.get('nuclei_distance_set', 80)
+    cell_distance_set = kwargs.get('cell_distance_set', 100)
     mos_distance_set = kwargs.get('mos_distance_set', 100)
     cell_intensity_thresh = kwargs.get('cell_intensity_thresh', 8)
 
@@ -557,34 +600,45 @@ def main(all_data_path, **kwargs):
             if not os.path.isdir(save_path):
                 os.mkdir(save_path)
 
+        matching_files_nuclei_org, matching_files_nuclei_color = Get_Image_Data(data_path, Sample_list[s_i], 4)
+        nuclei_mask = cv2.imread(matching_files_nuclei_org[0])
+        nuclei_mask_contour = cv2.imread(matching_files_nuclei_color[0])
+        assert nuclei_mask is not None, "file for nuclei could not be read, check with os.path.exists()"
+
         matching_files_cell_org, matching_files_cell_color = Get_Image_Data(data_path, Sample_list[s_i], 1)
         cell_mask = cv2.imread(matching_files_cell_org[0])
         cell_mask_contour = cv2.imread(matching_files_cell_color[0])
-        assert cell_mask is not None, "file could not be read, check with os.path.exists()"
+        assert cell_mask is not None, "file for Neutrophil could not be read, check with os.path.exists()"
 
+        # mos_mask = cv2.imread(data_path + 'Airyscan_63x_Tom20-488_Splenic neutrophils_1739_TG_4_c3_ORG.jpg')
         matching_files_mos_org, matching_files_mos_color = Get_Image_Data(data_path, Sample_list[s_i], 3)
         mos_mask = cv2.imread(matching_files_mos_org[0])
         mos_mask_contour = cv2.imread(matching_files_mos_color[0])
-        assert mos_mask is not None, "file could not be read, check with os.path.exists()"
+        assert mos_mask is not None, "file for Mitochondria could not be read, check with os.path.exists()"
 
         # Preprocessing the input image ================================================================================
 
+        nuclei_mask_ORG = nuclei_mask.copy()
+        nuclei_mask_ORG = cv2.cvtColor(nuclei_mask_ORG, cv2.COLOR_BGR2GRAY)
         cell_mask_ORG = cell_mask.copy()
         cell_mask_ORG = cv2.cvtColor(cell_mask_ORG, cv2.COLOR_BGR2GRAY)
 
         # Use the HSV to enhance brightness for initial segmentation (Only for cells)
         cell_mask = Brightness_Enhance(cell_mask)
 
+        nuclei_mask = cv2.cvtColor(nuclei_mask, cv2.COLOR_BGR2GRAY)
         cell_mask = cv2.cvtColor(cell_mask, cv2.COLOR_BGR2GRAY)
         mos_mask = cv2.cvtColor(mos_mask, cv2.COLOR_BGR2GRAY)
 
-        print('Processing Cell Marker of {} ============================================================================'.format(Sample_list[s_i]))
+        # print('Processing Cell Marker of {} ============================================================================'.format(Sample_list[s_i]))
+        update_progress('Processing Cell Marker of {} ------'.format(Sample_list[s_i]))
 
-        th_cell_mask, separate_line = Cell_Mask_Generate(cell_mask, cell_distance_set, element_region_size,  cell_intensity_thresh, cell_mask_ORG, cell_mask_contour,advanced_seg, save_sign, save_path, Sample_list[s_i])
+        th_cell_mask, separate_line = Cell_Mask_Generate(cell_mask, cell_distance_set, nuclei_mask, Suitable_Nuclei_distance_set, element_region_size, cell_intensity_thresh, cell_mask_ORG, cell_mask_contour, advanced_seg, save_sign, save_path, Sample_list[s_i])
 
         # Selected the mos regions =====================================================================================
 
-        print('Processing MOS Marker of {} ============================================================================='.format(Sample_list[s_i]))
+        # print('Processing MOS Marker of {} ============================================================================='.format(Sample_list[s_i]))
+        update_progress('Processing Mito Marker of {} ------'.format(Sample_list[s_i]))
 
         image_result, new_mask, sep_new_mask, label_num = Mito_Mask_Generate(mos_mask, th_cell_mask, separate_line, advanced_seg, save_sign, mos_distance_set, cell_mask_ORG, cell_mask_contour, save_path, Sample_list[s_i])
 
@@ -615,7 +669,3 @@ def main(all_data_path, **kwargs):
             df.to_csv(save_path + Sample_list[s_i] + '_analysis.csv', index=False)
         else:
             plt.show()
-
-
-
-
